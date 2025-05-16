2b704fbf3a6f42232e1178ee494f4829517ad7f0
Sergei Nikitin <kexa76@gmail.com>
2025-05-16 18:57:51 +0300

    run migrations on start

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,9 @@ async fn main() -> Result<(), Error> {
     let session_layer = SessionManagerLayer::new(session_store).with_secure(false);
 
     let postgres_pool = db::establish_postgres_connection();
+    //TODO: убрать expect
+    let connection = db::get_postgres_connection(&postgres_pool).expect("Could not get connection");
+    db::run_db_migrations(connection);
 
     let (router, open_api) = OpenApiRouter::with_openapi(ApiDoc::openapi())
         .nest("/api/v1/students", handlers::student_handler::router())
