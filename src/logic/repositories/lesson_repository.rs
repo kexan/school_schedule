f68200a156cba0702ae94d29ee8d58549b34d737
Sergei Nikitin <kexa76@gmail.com>
2025-05-31 10:51:55 +0300

    lesson repo load with relations method

--- src/logic/repositories/lesson_repository.rs
+++ src/logic/repositories/lesson_repository.rs
@@ -1,11 +1,17 @@
-use diesel::prelude::*;
+use diesel::{pg::Pg, prelude::*};
 
-use crate::db::PostgresPool;
-use crate::error::AppError;
-use crate::models::lesson::{Lesson, LessonWithRelations, NewLesson, UpdateLesson};
-use crate::models::student_group::StudentGroup;
-use crate::schema::lessons::{self};
-use crate::schema::student_groups;
+use crate::{
+    db::PostgresPool,
+    error::AppError,
+    models::{
+        lesson::{Lesson, LessonWithRelations, NewLesson, UpdateLesson},
+        student_group::StudentGroup,
+    },
+    schema::lessons::{self},
+    schema::student_groups,
+};
+
+use super::single_result;
 
 #[derive(Clone)]
 pub struct LessonRepository {
@@ -24,29 +30,19 @@ impl LessonRepository {
             .returning(lessons::id)
             .get_result::<i32>(&mut connection)?;
 
-        let (lesson, student_group) = lessons::table
-            .left_join(student_groups::table)
+        let query = lessons::table
             .filter(lessons::id.eq(lesson_id))
-            .select((Lesson::as_select(), Option::<StudentGroup>::as_select()))
-            .first::<(Lesson, Option<StudentGroup>)>(&mut connection)?;
+            .into_boxed();
 
-        Ok(LessonWithRelations {
-            lesson,
-            student_group,
-        })
+        single_result(self.load_with_relations(query)?)
     }
 
     pub fn get(&self, lesson_id: i32) -> Result<LessonWithRelations, AppError> {
-        let mut connection = self.pool.get()?;
-        let (lesson, student_group) = lessons::table
-            .left_join(student_groups::table)
+        let query = lessons::table
             .filter(lessons::id.eq(lesson_id))
-            .select((Lesson::as_select(), Option::<StudentGroup>::as_select()))
-            .first::<(Lesson, Option<StudentGroup>)>(&mut connection)?;
-        Ok(LessonWithRelations {
-            lesson,
-            student_group,
-        })
+            .into_boxed();
+
+        single_result(self.load_with_relations(query)?)
     }
 
     pub fn get_lessons_by_group_id(&self, id: i32) -> Result<Vec<Lesson>, AppError> {
@@ -67,20 +63,34 @@ impl LessonRepository {
             .set(&updated_lesson)
             .execute(&mut connection)?;
 
-        let (lesson, student_group) = lessons::table
-            .left_join(student_groups::table)
+        let query = lessons::table
             .filter(lessons::id.eq(lesson_id))
-            .select((Lesson::as_select(), Option::<StudentGroup>::as_select()))
-            .first::<(Lesson, Option<StudentGroup>)>(&mut connection)?;
+            .into_boxed();
 
-        Ok(LessonWithRelations {
-            lesson,
-            student_group,
-        })
+        single_result(self.load_with_relations(query)?)
     }
 
     pub fn delete(&self, lesson_id: i32) -> Result<usize, AppError> {
         let mut connection = self.pool.get()?;
         Ok(diesel::delete(lessons::table.find(lesson_id)).execute(&mut connection)?)
+    }
+
+    fn load_with_relations(
+        &self,
+        query: lessons::BoxedQuery<'_, Pg>,
+    ) -> Result<Vec<LessonWithRelations>, AppError> {
+        let mut connection = self.pool.get()?;
+        let results = query
+            .left_join(student_groups::table)
+            .select((Lesson::as_select(), Option::<StudentGroup>::as_select()))
+            .load::<(Lesson, Option<StudentGroup>)>(&mut connection)?
+            .into_iter()
+            .map(|(lesson, student_group)| LessonWithRelations {
+                lesson,
+                student_group,
+            })
+            .collect();
+
+        Ok(results)
     }
 }
