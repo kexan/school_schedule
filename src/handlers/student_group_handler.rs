9e439942aaadd3e6aa355a323a3b3dd10ca92ef2
Sergei Nikitin <kexa76@gmail.com>
2025-05-31 12:04:31 +0300

    student group handler docs

--- src/handlers/student_group_handler.rs
+++ src/handlers/student_group_handler.rs
@@ -30,7 +30,30 @@ pub fn router() -> OpenApiRouter<AppState> {
     OpenApiRouter::new().merge(dont_need_permissions)
 }
 
-#[utoipa::path(post, path = "/", request_body = NewStudentGroup)]
+/// Создание новой учебной группы
+///
+/// Этот эндпоинт позволяет создать новую учебную группу в базе данных.
+///
+/// ### Входные данные:
+/// - `direction`: Направление обучения (необязательное поле)
+/// - `free_spots`: Количество свободных мест (обязательное поле)
+/// - `teacher_id`: ID преподавателя (необязательное поле)
+///
+/// ### Ответы:
+/// - **201 Created**: Группа успешно создана. Возвращает данные созданной группы с преподавателем.
+/// - **400 Bad Request**: Неверные входные данные (например, отсутствует обязательное поле).
+/// - **500 Internal Server Error**: Внутренняя ошибка сервера.
+#[utoipa::path(
+    post,
+    path = "/",
+    request_body = NewStudentGroup,
+    responses(
+        (status = 201, body = StudentGroupWithRelations, description = "Группа успешно создана"),
+        (status = 400, description = "Неверные входные данные"),
+        (status = 500, description = "Внутренняя ошибка сервера")
+    ),
+    tag = "StudentGroup"
+)]
 async fn create_student_group(
     State(student_group_service): State<StudentGroupService>,
     Json(new_student_group): Json<NewStudentGroup>,
@@ -40,27 +63,72 @@ async fn create_student_group(
     Ok(Json(new_student_group))
 }
 
+/// Создание урока для учебной группы
+///
+/// Этот эндпоинт позволяет создать новый урок для конкретной учебной группы.
+///
+/// ### Параметры:
+/// - `id`: ID учебной группы (обязательный путь)
+///
+/// ### Входные данные:
+/// - `topic`: Название предмета (обязательное поле)
+/// - `scheduled_at`: Дата проведения урока (обязательное поле)
+/// -  Поле `student_group_id` будет проигнорировано, даже если передано - в этом эндпоинте всегда
+/// используется айди группы указанный в пути.
+///
+/// ### Ответы:
+/// - **201 Created**: Урок успешно создан. Возвращает данные созданного урока.
+/// - **400 Bad Request**: Неверные входные данные.
+/// - **500 Internal Server Error**: Внутренняя ошибка сервера.
 #[utoipa::path(
     post,
     path = "/{id}/lessons",
-    params(("id" = i32, Path, description = "ID группы учеников для которой создаем урок")),
-    request_body = NewLesson
+    params(
+        ("id" = i32, Path, description = "ID группы учеников для которой создаем урок")
+    ),
+    request_body = NewLesson,
+    responses(
+        (status = 201, body = LessonWithRelations, description = "Урок успешно создан"),
+        (status = 400, description = "Неверные входные данные"),
+        (status = 500, description = "Внутренняя ошибка сервера")
+    ),
+    tag = "StudentGroup"
 )]
 async fn create_lesson_for_student_group(
     State(lesson_service): State<LessonService>,
     Path(student_group_id): Path<i32>,
     Json(mut new_lesson): Json<NewLesson>,
 ) -> Result<Json<LessonWithRelations>, AppError> {
-    info!(
-        "Creating new lesson for student group with ID {}",
-        student_group_id
-    );
+    info!("Creating new lesson for student group");
     new_lesson.student_group_id = Some(student_group_id);
     let new_lesson = lesson_service.create(new_lesson)?;
     Ok(Json(new_lesson))
 }
 
-#[utoipa::path(get, path = "/{id}", params(("id" = i32, Path, description = "ID запрашиваемой группы учеников")))]
+/// Получение учебной группы по ID
+///
+/// Этот эндпоинт позволяет получить данные конкретной учебной группы по ее идентификатору.
+///
+/// ### Параметры:
+/// - `id`: ID группы (обязательный путь)
+///
+/// ### Ответы:
+/// - **200 OK**: Данные группы успешно получены.
+/// - **404 Not Found**: Группа с указанным ID не найдена.
+/// - **500 Internal Server Error**: Внутренняя ошибка сервера.
+#[utoipa::path(
+    get,
+    path = "/{id}",
+    params(
+        ("id" = i32, Path, description = "ID запрашиваемой группы учеников")
+    ),
+    responses(
+        (status = 200, body = StudentGroupWithRelations, description = "Данные группы успешно получены"),
+        (status = 404, description = "Группа не найдена"),
+        (status = 500, description = "Внутренняя ошибка сервера")
+    ),
+    tag = "StudentGroup"
+)]
 async fn get_student_group(
     State(student_group_service): State<StudentGroupService>,
     Path(student_group_id): Path<i32>,
@@ -70,10 +138,29 @@ async fn get_student_group(
     Ok(Json(student_group))
 }
 
+/// Получение всех уроков для учебной группы
+///
+/// Этот эндпоинт позволяет получить список всех уроков, связанных с конкретной учебной группой.
+///
+/// ### Параметры:
+/// - `id`: ID группы (обязательный путь)
+///
+/// ### Ответы:
+/// - **200 OK**: Список уроков успешно получен.
+/// - **404 Not Found**: Группа с указанным ID не найдена.
+/// - **500 Internal Server Error**: Внутренняя ошибка сервера.
 #[utoipa::path(
     get,
     path = "/{id}/lessons",
-    params(("id" = i32, Path, description = "ID группы учеников для которой запрашиваем уроки"))
+    params(
+        ("id" = i32, Path, description = "ID группы учеников для которой запрашиваем уроки")
+    ),
+    responses(
+        (status = 200, body = Vec<Lesson>, description = "Список уроков успешно получен"),
+        (status = 404, description = "Группа не найдена"),
+        (status = 500, description = "Внутренняя ошибка сервера")
+    ),
+    tag = "StudentGroup"
 )]
 async fn get_lessons_for_student_group(
     State(lessons_service): State<LessonService>,
@@ -84,11 +171,37 @@ async fn get_lessons_for_student_group(
     Ok(Json(lessons))
 }
 
+/// Обновление существующей учебной группы
+///
+/// Этот эндпоинт позволяет обновить данные учебной группы по ее идентификатору.
+///
+/// ### Параметры:
+/// - `id`: ID группы (обязательный путь)
+///
+/// ### Входные данные:
+/// - `direction`: Новое направление обучения (необязательное поле)
+/// - `free_spots`: Новое количество свободных мест (необязательное поле)
+/// - `teacher_id`: Новый ID преподавателя (необязательное поле)
+///
+/// ### Ответы:
+/// - **200 OK**: Данные группы успешно обновлены.
+/// - **404 Not Found**: Группа с указанным ID не найдена.
+/// - **400 Bad Request**: Неверные входные данные.
+/// - **500 Internal Server Error**: Внутренняя ошибка сервера.
 #[utoipa::path(
     put,
     path = "/{id}",
-    params(("id" = i32, Path, description = "ID Группы учеников которую требуется обновить")),
-    request_body = UpdateStudentGroup
+    params(
+        ("id" = i32, Path, description = "ID группы учеников которую требуется обновить")
+    ),
+    request_body = UpdateStudentGroup,
+    responses(
+        (status = 200, body = StudentGroupWithRelations, description = "Данные группы успешно обновлены"),
+        (status = 404, description = "Группа не найдена"),
+        (status = 400, description = "Неверные входные данные"),
+        (status = 500, description = "Внутренняя ошибка сервера")
+    ),
+    tag = "StudentGroup"
 )]
 async fn update_student_group(
     State(student_group_service): State<StudentGroupService>,
@@ -101,7 +214,30 @@ async fn update_student_group(
     Ok(Json(updated_student_group))
 }
 
-#[utoipa::path(delete, path = "/{id}", params(("id" = i32, Path, description = "ID Группы учеников которую требуется удалить")))]
+/// Удаление учебной группы
+///
+/// Этот эндпоинт позволяет удалить существующую учебную группу по ее идентификатору.
+///
+/// ### Параметры:
+/// - `id`: ID группы (обязательный путь)
+///
+/// ### Ответы:
+/// - **200 OK**: Группа успешно удалена.
+/// - **404 Not Found**: Группа с указанным ID не найдена.
+/// - **500 Internal Server Error**: Внутренняя ошибка сервера.
+#[utoipa::path(
+    delete,
+    path = "/{id}",
+    params(
+        ("id" = i32, Path, description = "ID группы учеников которую требуется удалить")
+    ),
+    responses(
+        (status = 200, body = String, description = "Группа успешно удалена"),
+        (status = 404, description = "Группа не найдена"),
+        (status = 500, description = "Внутренняя ошибка сервера")
+    ),
+    tag = "StudentGroup"
+)]
 async fn delete_student_group(
     State(student_group_service): State<StudentGroupService>,
     Path(student_group_id): Path<i32>,
